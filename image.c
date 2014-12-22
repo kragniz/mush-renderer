11f05c77660c380d66541a2f1ea6aaa2ec234921
Louis Taylor <kragniz@gmail.com>
2014-12-22 02:15:35 +0000

    Add image_get_samples function

--- image.c
+++ image.c
@@ -13,6 +13,10 @@ struct image *image_new(const int w, const int h) {
     return image;
 }
 
+struct samples *image_get_samples(const struct image *im, const int x, const int y) {
+    return &(im->samples[y*im->w + x]);
+}
+
 void image_to_ppm(const struct image *im, FILE *fp) {
     fprintf(fp, "P6\n%i %i\n", im->w, im->h);
 }
--- image.h
+++ image.h
@@ -11,6 +11,8 @@ struct image {
 
 struct image *image_new(const int w, const int h);
 
+struct samples *image_get_samples(const struct image *im, const int x, const int y);
+
 void image_to_ppm(const struct image *im, FILE *fp);
 
 #endif
